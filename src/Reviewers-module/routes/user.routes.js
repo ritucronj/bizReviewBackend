f6f063203d372a7491fa87cac22ba72ee75c1a4c
Devashish514 <devagosw193@gmail.com>
2023-02-21 18:50:25 +0530

    login/fix

--- src/Reviewers-module/routes/user.routes.js
+++ src/Reviewers-module/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUserWithEmail, getAllUsers, ssoRegister } = require("../controller/user.controller");
+const { registerUserWithEmail, getAllUsers, ssoRegisterAndLogin } = require("../controller/user.controller");
 const router = express.Router();
 
 router.get("/hello", (req, res) => {
@@ -7,7 +7,7 @@ router.get("/hello", (req, res) => {
 });
 
 router.post('/register', registerUserWithEmail);
-router.post("/registerSSO", ssoRegister);
+router.post("/registerSSO", ssoRegisterAndLogin);
 router.get("/getAll", getAllUsers);
 
 module.exports = router;
